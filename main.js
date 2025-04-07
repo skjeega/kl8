fe0e494bd7a873300c8ec3752572f174edeb7be0
skjeega <agra.jeerica@gmail.com>
2025-04-07 09:53:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -18,28 +18,3 @@ fails.addEventListener("change", function(){
   document.getElementById("izvade").innerHTML = "Faila nosaukums: " + nosaukums + "<br>" + "Faila izmērs: " + izmers + " baiti"
 });
 
-// Zīmēšanas spēles programma
-// definē mainīgos, kas norāda, ka tiks strādāts ar canvas elementu un norāda zīmēšanas kontekstu
-let laukums = document.getElementById("laukums");
-let ctx = laukums.getContext("2d");
-
-// Iestata zīmēšanas laukuma izmērus - platums un augstums
-laukums.width = 300;
-laukums.height = 300;
-
-// Izveido nepieciešamos programmas mainīgos
-let x, y, platums, augstums, krasa;
-// definē mainīgo, kas norāda, ka tiks strādāts ar pogas elementu
-let poga = document.getElementById("poga");
-// Pievieno pogas elementam notikumu klausītāju, kas izpildīs kodu, kad uz pogas tiks noklikšķināts
-poga.addEventListener("click", function(){
-  x = document.getElementById("x").value; // nolasa ievades lauka vērtību
-  y = document.getElementById("y").value;// nolasa ievades lauka vērtību
-  augstums = document.getElementById("augstums").value;// nolasa ievades lauka vērtību
-  platums = document.getElementById("platums").value;// nolasa ievades lauka vērtību
-  krasa = document.getElementById("krāsa").value;// nolasa krāsas ievades lauka vērtību
-
-  
-  ctx.fillStyle = krasa;// iestata četrstūra zīmēšanas krāsu
-  ctx.fillRect(x, y, augstums, platums) // uzzīmē četrstūri, kura vērtības tiek nolasītas no ievades laukiem
-})
